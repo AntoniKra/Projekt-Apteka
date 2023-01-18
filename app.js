439eeaf7a9b944e68c7b2f9a6295058c4766a0d7
KLesiu <kubaxl222@gmail.com>
2023-01-18 21:56:52 +0100

    Add saturday and sunday option at clock function. Delete nav bug from style.css

--- app.js
+++ app.js
@@ -9,24 +9,42 @@ const days = [
   "Sobota",
   "Niedziela ",
 ];
-const closeHour = 19;
-const closeMinutes = 59;
-const closeSeconds = 60;
+let closeHour = 0;
+let closeMinutes = 0;
+let closeSeconds = 0;
 const clock = () => {
   const dateTime = new Date();
-
   let day = dateTime.getDay();
+  let currentDay;
   for (let i = 0; i < days.length; i++) {
     if (day == i) {
-      day = days[i];
+      currentDay = days[i];
     }
   }
   let hours = dateTime.getHours();
   let minutes = dateTime.getMinutes();
   let seconds = dateTime.getSeconds();
-  document.querySelector(".day").innerHTML = day;
-  document.querySelector(".hours").innerHTML = closeHour - hours;
-  document.querySelector(".minutes").innerHTML = closeMinutes - minutes;
-  document.querySelector(".seconds").innerHTML = closeSeconds - seconds;
+  if (day < 6) {
+    closeHour = 19;
+    closeMinutes = 59;
+    closeSeconds = 59;
+  } else if (day == 6) {
+    closeHour = 14;
+    closeMinutes = 59;
+    closeSeconds = 59;
+  }
+  let currentHour = closeHour - hours;
+  let currentMinute = closeMinutes - minutes;
+  let currentSecond = closeSeconds - seconds;
+  document.querySelector(".day").innerHTML = currentDay;
+  document.querySelector(".hours").innerHTML = currentHour;
+  document.querySelector(".minutes").innerHTML = currentMinute;
+  document.querySelector(".seconds").innerHTML = currentSecond;
+  if (day == 7) {
+    document.querySelector(".day").innerHTML = currentDay;
+    document.querySelector(".hours").innerHTML = "Zamknięte";
+    document.querySelector(".minutes").innerHTML = "";
+    document.querySelector(".seconds").innerHTML = "";
+  }
 };
 setInterval(clock, 10);
